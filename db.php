97633e64443e09d5d87a6372db63f34ef757bf1e
Burtov4ik <118672866+Burtov4ik@users.noreply.github.com>
2025-07-29 23:17:07 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "127.0.0.1";
+$servername = "db";
 $username = "root";
 $password = "kali";
 $dbName = "first";
@@ -36,7 +36,8 @@ if(!mysqli_query($link, $sql)) {
 $sql = "CREATE TABLE IF NOT EXISTS posts(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(20) NOT NULL,
-  main_text VARCHAR(400) NOT NULL
+  main_text VARCHAR(400) NOT NULL,
+  image_path VARCHAR(255) NULL
 )";
 
 if(!mysqli_query($link, $sql)) {
